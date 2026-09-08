42e0fe51fedd0ad1ba030b99cb7013c655045a73
ankitkumarchoudhary01 <choudharyankit0852@gmail.com>
2026-09-09 03:16:32 +0530

    v-1.0.1 Deployment Links

--- server/src/index.js
+++ server/src/index.js
@@ -22,7 +22,7 @@ const startServer = async () => {
 
     const server = app.listen(PORT, () => {
       console.log(
-        `[Server] Running in ${process.env.NODE_ENV || 'development'} mode on http://localhost:${PORT}`
+        `[Server] Running in ${process.env.NODE_ENV || 'development'} mode on https://psychological-clock-urdu.vercel.app/`
       );
     });
 
